dccdb2be502f415c42e775ccf5dcccff7764239d
Lailatul Qadri Zakaria <61860775+lailatulqadri@users.noreply.github.com>
2024-06-12 12:20:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def predict_sentiment(model, tokenizer, text):
     probabilities = torch.softmax(logits, dim=1)
     predicted_class = torch.argmax(probabilities, dim=1).item()
     sentiment = 'positive' if predicted_class > 0 else 'negative'
-    return sentiment
+    return predicted_class
 
 
 model, tokenizer = load_model()
